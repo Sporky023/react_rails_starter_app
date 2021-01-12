dcedd9740c64be90fbbb2075695bd6a43e3383b7
Luke Griffiths <wlgriffiths@gmail.com>
2021-01-12 01:56:56 -0700

    lib: add redux-thunk and configure

--- client/apps/hello_world/store/configure_store.js
+++ client/apps/hello_world/store/configure_store.js
@@ -2,8 +2,9 @@ import configureStore from 'atomic-fuel/libs/store/configure_store';
 import rootReducer from '../reducers/index';
 import API from '../middleware/api';
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-const middleware = [API, logger];
+const middleware = [API, thunk, logger];
 
 // This file just exports the default configure store. If modifications are needed
 // make the modifications in this file by extending the configureStore
